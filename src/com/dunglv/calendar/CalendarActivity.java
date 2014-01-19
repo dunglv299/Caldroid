ba3f4213d0fdd39fba46fbea247ab81cfff08340
dunglv299 <dunglv299@gmail.com>
2014-01-19 22:19:38 +0700

    create UI

--- src/com/dunglv/calendar/CalendarActivity.java
+++ src/com/dunglv/calendar/CalendarActivity.java
@@ -1,28 +1,53 @@
 package com.dunglv.calendar;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 import android.annotation.SuppressLint;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.caldroid.caldroidcustom.CaldroidFragment;
 import com.caldroid.caldroidcustom.CaldroidListener;
 
 @SuppressLint("SimpleDateFormat")
-public class CalendarActivity extends FragmentActivity {
+public class CalendarActivity extends SherlockFragmentActivity {
 	private static final String TAG = "CaldroidSampleActivity";
 	private CaldroidFragment caldroidFragment;
+	private SimpleDateFormat formatter;
+	// nav drawer title
+	private CharSequence mDrawerTitle;
+
+	// used to store app title
+	private CharSequence mTitle;
+
+	// slide menu items
+	private String[] navMenuTitles;
+	private TypedArray navMenuIcons;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private ArrayList<NavDrawerItem> navDrawerItems;
+	private NavDrawerListAdapter adapter;
+	private ActionBar actionBar;
 
 	private void initData() {
 		Calendar cal = Calendar.getInstance();
-
 		// Min date is last 7 days
 		cal.add(Calendar.DATE, -17);
 		Date blueDate = cal.getTime();
@@ -45,28 +70,12 @@ public class CalendarActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-
+		initNavigationDrawer();
 		// Setup caldroid fragment
 		// **** If you want normal CaldroidFragment, use below line ****
 		caldroidFragment = new CaldroidFragment();
-		// If Activity is created after rotation
-		if (savedInstanceState != null) {
-			caldroidFragment.restoreStatesFromKey(savedInstanceState,
-					"CALDROID_SAVED_STATE");
-		}
-		// If activity is created from fresh
-		else {
-			Bundle args = new Bundle();
-			Calendar cal = Calendar.getInstance();
-			args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-			args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-			args.putBoolean(CaldroidFragment.ENABLE_SWIPE, false);
-			args.putBoolean(CaldroidFragment.SIX_WEEKS_IN_CALENDAR, true);
-			args.putInt(CaldroidFragment.START_DAY_OF_WEEK,
-					CaldroidFragment.SUNDAY); // Tuesday
-			caldroidFragment.setArguments(args);
-		}
 		setUpCaldroidFragment();
 		// Attach to the activity
 		FragmentTransaction t = getSupportFragmentManager().beginTransaction();
@@ -77,39 +86,184 @@ public class CalendarActivity extends FragmentActivity {
 	}
 
 	private void setUpCaldroidFragment() {
-		final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
-		// Setup listener
-		final CaldroidListener listener = new CaldroidListener() {
-
-			@Override
-			public void onSelectDate(Date date, View view) {
-				Log.e(TAG, formatter.format(date));
-
-			}
-
-			@Override
-			public void onChangeMonth(int month, int year) {
-				String text = "month: " + month + " year: " + year;
-				Log.e(TAG, text);
-			}
-
-			@Override
-			public void onLongClickDate(Date date, View view) {
-				Toast.makeText(getApplicationContext(),
-						"Long click " + formatter.format(date),
-						Toast.LENGTH_SHORT).show();
-				Log.e(TAG, "Long click");
-			}
-
-			@Override
-			public void onCaldroidViewCreated() {
-				if (caldroidFragment.getLeftArrowButton() != null) {
-					Log.e(TAG, "Caldroid view is created");
-				}
-			}
-
-		};
-		// Setup Caldroid
+		formatter = new SimpleDateFormat("dd MMM yyyy");
+		Bundle args = new Bundle();
+		Calendar cal = Calendar.getInstance();
+		args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+		args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+		args.putBoolean(CaldroidFragment.ENABLE_SWIPE, true);
+		args.putBoolean(CaldroidFragment.SIX_WEEKS_IN_CALENDAR, true);
+		args.putInt(CaldroidFragment.START_DAY_OF_WEEK, CaldroidFragment.SUNDAY); // Tuesday
+		caldroidFragment.setArguments(args);
 		caldroidFragment.setCaldroidListener(listener);
+	}
+
+	// Setup listener
+	final CaldroidListener listener = new CaldroidListener() {
+
+		@Override
+		public void onSelectDate(Date date, View view) {
+			Log.e(TAG, formatter.format(date));
+		}
+
+		@Override
+		public void onChangeMonth(int month, int year) {
+			String text = "month: " + month + " year: " + year;
+			Log.e(TAG, text);
+		}
+
+		@Override
+		public void onLongClickDate(Date date, View view) {
+			Toast.makeText(getApplicationContext(),
+					"Long click " + formatter.format(date), Toast.LENGTH_SHORT)
+					.show();
+			Log.e(TAG, "Long click " + formatter.format(date));
+		}
+
+		@Override
+		public void onCaldroidViewCreated() {
+			if (caldroidFragment.getLeftArrowButton() != null) {
+				Log.e(TAG, "Caldroid view is created");
+			}
+		}
+	};
+
+	/**
+	 * Init left menu navigation drawer
+	 */
+	public void initNavigationDrawer() {
+		mTitle = mDrawerTitle = getTitle();
+		// load slide menu items
+		navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+		// nav drawer icons from resources
+		navMenuIcons = getResources()
+				.obtainTypedArray(R.array.nav_drawer_icons);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+		navDrawerItems = new ArrayList<NavDrawerItem>();
+
+		// adding nav drawer items to array
+		// Home
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons
+				.getResourceId(0, -1)));
+		// Find People
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons
+				.getResourceId(1, -1)));
+
+		// Recycle the typed array
+		navMenuIcons.recycle();
+
+		mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+
+		// setting the nav drawer list adapter
+		adapter = new NavDrawerListAdapter(getApplicationContext(),
+				navDrawerItems);
+		mDrawerList.setAdapter(adapter);
+
+		// enabling action bar app icon and behaving it as toggle button
+		actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setIcon(R.drawable.ic_actionbar);
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, // nav menu toggle icon
+				R.string.app_name, // nav drawer open - description for
+									// accessibility
+				R.string.app_name // nav drawer close - description for
+									// accessibility
+		) {
+			public void onDrawerClosed(View view) {
+				actionBar.setTitle(mTitle);
+				// calling onPrepareOptionsMenu() to show action bar icons
+				supportInvalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				actionBar.setTitle(mDrawerTitle);
+				// calling onPrepareOptionsMenu() to hide action bar icons
+				supportInvalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+	}
+
+	/**
+	 * Slide menu item click listener
+	 * */
+	private class SlideMenuClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			// display view for selected nav drawer item
+			displayView(position);
+		}
+	}
+
+	/**
+	 * Diplaying fragment view for selected nav drawer list item
+	 * */
+	private void displayView(int position) {
+		mDrawerList.setItemChecked(position, true);
+		mDrawerList.setSelection(position);
+		setTitle(navMenuTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		actionBar.setTitle(mTitle);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar actions click
+
+		switch (item.getItemId()) {
+		// toggle nav drawer on selecting action bar app icon/title
+		case android.R.id.home:
+			// if nav drawer is opened, hide the action items
+			boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+			if (drawerOpen) {
+				mDrawerLayout.closeDrawer(mDrawerList);
+			} else {
+				mDrawerLayout.openDrawer(mDrawerList);
+			}
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/* *
+	 * Called when invalidateOptionsMenu() is triggered
+	 */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		return super.onPrepareOptionsMenu(menu);
 	}
 }
